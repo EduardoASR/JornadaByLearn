cbf4e86786de291d0f862962986925b39e67f9c9
EduardoASR <77114420+EduardoASR@users.noreply.github.com>
2021-01-07 17:34:21 -0300

    Add files via upload

---
+++ temperatura.py
@@ -0,0 +1,8 @@
+temperatura = 'frio'
+
+if temperatura == 'quente':
+  print('Vamos tomar sorvete de flocos!')
+  print('Vamos para a praia')
+else:
+  print('Vamos comer chocolate')
+  print('Vamos ao cinema')
