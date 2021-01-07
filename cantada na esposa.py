9cf9a066528993a0e40361982b79bf479ef17870
EduardoASR <77114420+EduardoASR@users.noreply.github.com>
2021-01-07 20:05:30 -0300

    Add files via upload
    Uso das funções de formulário e "se"

---
+++ cantada na esposa.py
@@ -0,0 +1,10 @@
+#@title TESTE DE PERSONALIDADE { run: "auto", vertical-output: true }
+DATA_DE_NASCIMENTO = "1981-04-12" #@param {type:"date"}
+nome = input ('nome')
+sobrenome = input ('Sobrenome')
+Idade = input ('sua_idade')
+if nome == 'GISLAINE':
+  if sobrenome == 'RIBEIRO':
+    print ('A MULHER MAIS LINDA DO MUNDO')
+else:
+  print ('não é a minha esposa')
